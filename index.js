5f4a7768ae34d030201ed95cfa2490ff0f73a2e2
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-14 22:04:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name)
+}
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens
+}
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens
+}
+function removeLastKitten() {
+  var newKittens = 
+}
